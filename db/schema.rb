6134efa74bc7bb4a61f337b9f2725922e6af1d25
Alexander Kalik <alexander.kalik@superion.com>
2018-06-04 16:36:58 -0400

    service type added to appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_203118) do
+ActiveRecord::Schema.define(version: 2018_06_04_203632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_203118) do
     t.string "petbreed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "service_type"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
