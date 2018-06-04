80a477f853ff8897ebb7596c67ef631dd129337c
Alexander Kalik <alexander.kalik@superion.com>
2018-06-04 16:31:46 -0400

    appointment table added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_202910) do
+ActiveRecord::Schema.define(version: 2018_06_04_203118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "time"
+    t.string "petname"
+    t.string "petbreed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2018_06_04_202910) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "appointments", "users"
 end
