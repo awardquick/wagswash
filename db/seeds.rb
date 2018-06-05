af0be6298f6359f28c6af767462355d9f8853cf5
Alexander Kalik <alexander.kalik@superion.com>
2018-06-05 12:56:52 -0400

    basic seed file added to application - run rake db:seed to seed your database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: "Alex Kalik", address: "4112 Pershing Pointe Pl", phone: "3017777777", email: "alex@example.com")
+
+Pet.create(pet_name: "Whiskey", pet_breed: "Labrador", user_id: 1)
+
+Employee.create(name: "Jack")
+
+Employee.create(name: "Jill")
+
+Schedule.create(employee_id: 1)
+Schedule.create(employee_id: 2)
